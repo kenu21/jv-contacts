d926f785111fc30a33bdcab405532152cf815c1a
Iurii <kenu21@gmail.com>
2019-11-20 12:23:48 +0200

    Initial commit

---
+++ src/test/java/tech/giver/contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.giver.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
