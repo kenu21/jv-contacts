0ff3d6e6d4f049aedafdc54a1bfbbfd69c4eaac6
Iurii <kenu21@gmail.com>
2019-11-22 11:09:27 +0200

    added changing users

--- src/main/java/tech/giver/contacts/util/Convert.java
+++ src/main/java/tech/giver/contacts/util/Convert.java
@@ -6,6 +6,9 @@ import tech.giver.contacts.entity.User;
 public class Convert {
     public static User userDtoToUser(UserDto userDto) {
         User user = new User(userDto.getUsername(), userDto.getPassword());
+        user.setFirstname(userDto.getFirstname());
+        user.setLastname(userDto.getLastname());
+        user.setRoles(userDto.getRoles());
         return user;
     }
 }
